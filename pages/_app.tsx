caf87c28090fb6dbc59d4f87885d0c29983ee03e
lohith warlock <lohxt.space@gmail.com>
2023-02-18 19:02:23 +0530

    feat: added fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import Script from "next/script";
+import { useEffect } from "react";
 import { Inter } from "@next/font/google";
+import * as Fathom from "fathom-client";
 import { SessionProvider } from "next-auth/react";
 import { SessionProviderProps } from "next-auth/react";
 import Layout from "@/components/layout/layout";
@@ -13,6 +16,25 @@ interface CustomAppProps extends AppProps {
 }
 
 const App = ({ Component, pageProps }: CustomAppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load("XHWSZGQP", {
+      includedDomains: ["lohxt.space"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   return (
     <>
       <Script src="https://app.lemonsqueezy.com/js/lemon.js" />
